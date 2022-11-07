255294f94a29089dd32f7cb836a4355d924aac1d
felix-richard <105433778+felix-richard@users.noreply.github.com>
2022-11-07 17:42:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,15 +12,22 @@ console.log(event);
 var Content =event.results[0][0].transcript;
 console.log(Content);
 document.getElementById("textbox").innerHTML = Content;
+if(Content == "take my selfie")
+{
+    console.log("taking selfie ---");
 speak();
 }
-
+}
 function speak(){
 var synth = window.speechSynthesis;
-speak_data = document.getElementById ("textbox").value;
+speak_data = "Taking your selfie in  5 seconds" ;
 var utterThis = new SpeechSynthesisUtterance(speak_data);
 synth.speak(utterThis);
 Webcam.attach(camera);
+setTimeout(function(){
+take_snapshot();
+save();
+},5000);
 }
 Webcam.set({
     width:360,
@@ -30,3 +37,17 @@ Webcam.set({
 
 });
 camera= document.getElementById("camera");
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+      document.getElementById("result").innerHTML = "<img id='selfie_image' src ='"+data_uri+ "'>";
+    });
+}
+function save()
+{
+link = document.getElementById("link");
+image = document.getElementById("selfie_image").src ;
+link.href = image;
+link.click();
+
+}
